89dda5c2672313b1afa770c35264452e6fccbac3
Eric Busboom <eric@civicknowledge.com>
2025-06-27 22:49:59 +0000

    Update gravity and jump mechanics for improved gameplay dynamics; remove unused scratch notebook

--- lessons/01_Physics_for_Games/05_gravity_bounce.py
+++ lessons/01_Physics_for_Games/05_gravity_bounce.py
@@ -18,9 +18,9 @@ class GameSettings:
     square_color: tuple = (0, 0, 0)  # Black
     background_color: tuple = (255, 255, 255)  # White
     fps: int = 30
-    gravity: float = 60.0  # Acceleration due to gravity
-    jump_velocity_y: float = 200.0  # Initial jump velocity in y direction
-    jump_velocity_x: float = 100.0  # Initial jump velocity in x direction
+    gravity: float = 400.0  # Acceleration due to gravity
+    jump_velocity_y: float = 500.0  # Initial jump velocity in y direction
+    x_0: float = 100.0  # Initial x velocity
     d_t: float = 1.0/30  # Time step for physics calculations
 
 # Initialize Pygame
@@ -34,13 +34,13 @@ screen = pygame.display.set_mode((settings.screen_width, settings.screen_height)
 pygame.display.set_caption("Gravity Bounce")
 
 # Square starting position
-x_pos = 100
-y_pos = settings.screen_height - settings.square_size
+x = 100
+y = settings.screen_height - settings.square_size
 
 # Initial velocities  
-velocity_x = 0
-velocity_y = 0
-x_direction = 1  # Either 1 or -1, to keep track of direction after hitting the ground
+v_x = 0
+v_y = 0
+x_dir = 1  # Either 1 or -1, to keep track of direction after hitting the ground
 
 is_jumping = False
 
@@ -61,8 +61,8 @@ while running:
         # screen is y=0, and the bottom is y=screen_height. So, to go up,
         # we need to have a negative y velocity
         
-        velocity_y = -settings.jump_velocity_y
-        velocity_x = settings.jump_velocity_x * x_direction
+        v_y = -settings.jump_velocity_y
+        v_x = settings.x_0 * x_dir
         
         is_jumping = True
         
@@ -72,40 +72,46 @@ while running:
         # to make the square go up more slowly. Eventually, the square will have
         # a positive y velocity, and gravity will pull the square down.
 
-        velocity_y += settings.gravity * settings.d_t
+        v_y += settings.gravity * settings.d_t
         
         # Update the position with the velocity. Like with the velocity, we change
         # the position by adding the velocity, not setting it to the velocity, and
         # we change it a bit each frame.
-        y_pos += velocity_y * settings.d_t
-        x_pos += velocity_x * settings.d_t
+        y += v_y * settings.d_t
+        x += v_x * settings.d_t
         
     # If the square hits one side of the screen or the other, bounce the square
-    if x_pos <= 0 or x_pos + settings.square_size >= settings.screen_width:
-        velocity_x = -velocity_x
+    if x <= 0: # Hit the left side
+        x = 0
+        v_x = -v_x
+
+    if x + settings.square_size >= settings.screen_width: # hit the right side
+        x = settings.screen_width - settings.square_size
+        v_x = -v_x
         
-        # Update direction tracking
-        x_direction = -x_direction 
-        # This way is more reliable, since it will always be 1 or -1 and direction is tied to velocity
-        if velocity_x != 0:
-            x_direction = int(velocity_x / abs(velocity_x))
 
     # If the square hits the top of the screen, bounce the square
-    if y_pos <= 0:
-        velocity_y = -velocity_y
+    if y <= 0:
+        y = 0
+        v_y = -v_y
 
     # If the square hits the ground, stop the square from falling.
-    if y_pos + settings.square_size > settings.screen_height:
-        y_pos = settings.screen_height - settings.square_size
-        velocity_y = 0
-        velocity_x = 0
+    if y + settings.square_size > settings.screen_height:
+        y = settings.screen_height - settings.square_size
+
+        # Remember which way we were moving so we can jump in that direction later. 
+        x_dir = int(v_x / abs(v_x)) if v_x != 0 else 1
+
+        v_y = 0
+        v_x = 0
         is_jumping = False
+
 
     # Fill the screen with background color (clears previous frame)
     screen.fill(settings.background_color)
 
     # Draw the square
-    pygame.draw.rect(screen, settings.square_color, (x_pos, y_pos, settings.square_size, settings.square_size))
+    pygame.draw.rect(screen, settings.square_color, (x, y, settings.square_size, settings.square_size))
 
     # Update the display
     pygame.display.flip()
